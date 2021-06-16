6e5fd3de07fb43c475f070aca5048973163ba0be
proffl <157150278@qq.com>
2021-06-17 01:24:37 +0800

    BigInteger add works in main.cpp

--- Algorithm/main.cpp
+++ Algorithm/main.cpp
@@ -1,12 +1,29 @@
 #include <iostream>
 
 #include "BigInteger.h"
+
 int main() {
-    BigInteger b1("12345", 5);
-    BigInteger b2("12345", 5);
-    BigInteger* b3 = b1.add(b2);
+    char *a = "16549843132165467985641313216549879451613123131";
+    char *b = "165498431321654679856413132165498794516131231313564983169";
 
-    b3->print();
+    std::cout<< strlen(a) << std::endl;
 
+    char result[200];
+    memset(result, 0, sizeof(result));
+    int extra = 0;
+    for (int i = 0; i < strlen(a); i++) {
+        result[i] = a[i];
+    }
+    for (int j = 0; j < strlen(b); j++) {
+        int t = j < strlen(a)? result[j] - '0' : 0;
+        int intValue = t + b[j] - '0' + extra;
+        extra = intValue / 10;
+        result[j] = intValue % 10 + '0';
+    }
+    if (extra > 0) {
+        std::cout <<"I'm here" << std::endl;
+        result[strlen(a)] = extra + '0';
+    }
+    std::cout << result << std::endl;
     return 0;
 }
