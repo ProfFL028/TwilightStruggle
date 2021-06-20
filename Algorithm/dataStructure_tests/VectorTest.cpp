fb4b1f3ff7476c66d1fc63045099343f1e01e7ea
proffl <157150278@qq.com>
2021-06-20 19:32:30 +0800

    Make Vector work. add some simple test for vector

--- Algorithm/dataStructure_tests/VectorTest.cpp
+++ Algorithm/dataStructure_tests/VectorTest.cpp
@@ -1,12 +1,17 @@
 //
 // Created by Administrator on 2021/6/20.
 //
-#include "../dataStructure/Vector.h"
+#include "../dataStructure/Vector.cpp"
 
 int main() {
     Vector<char> v;
     for (int i = 0; i < 10; i++) {
         v.add(i + '0');
     }
+    // it will print 9876543210
+    v.print();
+
+    v.remove();
+    // it will print 876543210
     v.print();
 }
