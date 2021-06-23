f99f100b1b554f729415ea07fe5f4ace776074fd
proffl <157150278@qq.com>
2021-06-23 19:34:15 +0800

    count 1 in uint32_t

--- Algorithm/tests/Number1CounterInIntegerTest.cpp
+++ Algorithm/tests/Number1CounterInIntegerTest.cpp
@@ -1,4 +1,20 @@
 //
 // Created by Administrator on 2021/6/23.
 //
+#include <iostream>
+#include <bitset>
+#include "../leetcode/Number1CounterInInteger.cpp"
 
+int main() {
+    uint32_t n = 56;
+
+    std::cout << (n<<1) <<std::endl;
+    std::bitset<32> x(-n);
+    std::bitset<32> nn(n);
+    std::cout << nn << std::endl;
+    std::cout << x << std::endl;
+    std::cout << Number1CounterInInteger::hammingWeight(n) << std::endl;
+    std::cout << Number1CounterInInteger::hammingWeight2(n) << std::endl;
+
+    return 0;
+};
