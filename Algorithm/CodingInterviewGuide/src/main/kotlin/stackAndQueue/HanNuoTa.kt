bbb901a3091beee7f20a3febf9578224ec3a0cc3
proffl <157150278@.qq.com>
2021-11-07 00:26:06 +0800

    CodingInterviewGuide: HanNuoTa：Move like circle implementation move like binary code implementation

--- Algorithm/CodingInterviewGuide/src/main/kotlin/stackAndQueue/HanNuoTa.kt
+++ Algorithm/CodingInterviewGuide/src/main/kotlin/stackAndQueue/HanNuoTa.kt
@@ -2,6 +2,11 @@ package stackAndQueue
 
 import java.lang.RuntimeException
 
+/**
+ * 汉诺塔， https://en.wikipedia.org/wiki/Tower_of_Hanoi
+ *
+ */
+
 class HanNuoTa {
     var hnt1 = ArrayDeque<Int>()
     var hnt2 = ArrayDeque<Int>()
@@ -33,6 +38,7 @@ class HanNuoTa {
             }
         }
     }
+
     fun moveAToB(n: Int) {
         when (n) {
             1 -> println("move 1 from a to b")
@@ -50,6 +56,7 @@ class HanNuoTa {
             }
         }
     }
+
     fun moveBToC(n: Int) {
         when (n) {
             1 -> {
@@ -69,6 +76,7 @@ class HanNuoTa {
             }
         }
     }
+
     fun moveBToA(n: Int) {
         when (n) {
             1 -> {
@@ -88,6 +96,7 @@ class HanNuoTa {
             }
         }
     }
+
     fun moveCToB(n: Int) {
         when (n) {
             1 -> {
@@ -107,6 +116,7 @@ class HanNuoTa {
             }
         }
     }
+
     fun moveCToA(n: Int) {
         when (n) {
             1 -> {
@@ -126,9 +136,118 @@ class HanNuoTa {
             }
         }
     }
+
+    /**
+     * 1。如果N是偶数，则1按ABCABC。。。顺序移动。如果N是奇数，则1按ACBACB顺序移动
+     * 2。按顺序移动1
+     * 3。移动非1所在两根柱子，（只能有一种可以移动的方式）
+     * 4。如果没有全部到C柱子，回到第二步。
+     */
+    fun moveLikeACircle(n: Int) {
+        if (n <= 0)
+            throw RuntimeException("Error Input")
+
+        var stacks = arrayListOf<ArrayDeque<Int>>()
+        stacks.add(ArrayDeque())
+        stacks.add(ArrayDeque())
+        stacks.add(ArrayDeque())
+
+        for (i in 1..n) {
+            stacks[0].addFirst(i)
+        }
+
+        fun moveSingle(num: Int, from: Int, to: Int) {
+            stacks[to].add(stacks[from].removeLast())
+            println("move $num from $from to $to")
+            println("------>stack0:${stacks[0]}")
+            println("------>stack1:${stacks[1]}")
+            println("------>stack2:${stacks[2]}")
+        }
+
+        var dest = if (n % 2 == 0) 2 else 1
+        var onePosition = 0
+        while (stacks[dest].size != n) {
+            var prePosition = onePosition
+            onePosition = (onePosition + 1) % 3
+            moveSingle(1, prePosition, onePosition)
+            if (stacks[dest].size == n) {
+                break
+            }
+
+            when (onePosition) {
+                0 -> {
+                    val n1 = stacks[1].lastOrNull() ?: Int.MAX_VALUE
+                    val n2 = stacks[2].lastOrNull() ?: Int.MAX_VALUE
+                    if (n1 > n2) {
+                        moveSingle(n2, 2, 1)
+                    } else {
+                        moveSingle(n1, 1, 2)
+                    }
+                }
+                1 -> {
+                    val n1 = stacks[0].lastOrNull() ?: Int.MAX_VALUE
+                    val n2 = stacks[2].lastOrNull() ?: Int.MAX_VALUE
+                    if (n1 > n2) {
+                        moveSingle(n2, 2, 0)
+                    } else {
+                        moveSingle(n1, 0, 2)
+                    }
+                }
+                2 -> {
+                    val n1 = stacks[1].lastOrNull() ?: Int.MAX_VALUE
+                    val n2 = stacks[0].lastOrNull() ?: Int.MAX_VALUE
+                    if (n1 > n2) {
+                        moveSingle(n2, 0, 1)
+                    } else {
+                        moveSingle(n1, 1, 0)
+                    }
+                }
+            }
+        }
+    }
+
+    fun moveLikeBinaryCode(n: Int) {
+        if (n <= 0)
+            throw RuntimeException("Error Input")
+
+        var stacks = arrayListOf<ArrayDeque<Int>>()
+        stacks.add(ArrayDeque())
+        stacks.add(ArrayDeque())
+        stacks.add(ArrayDeque())
+
+        for (i in 1..n) {
+            stacks[0].addFirst(i)
+        }
+
+        fun moveSingle(from: Int, to: Int) {
+            var num = stacks[from].last()
+            stacks[to].add(stacks[from].removeLast())
+            println("move $num from $from to $to")
+            println("------>stack0:${stacks[0]}")
+            println("------>stack1:${stacks[1]}")
+            println("------>stack2:${stacks[2]}")
+        }
+
+        var oddSteps = if(n % 2 == 0) arrayListOf(0, 1, 2) else arrayListOf(0, 2, 1)
+        var evenSteps = if(n % 2 == 1) arrayListOf(0, 1, 2) else arrayListOf(0, 2, 1)
+
+        var step = 1
+        while (stacks[2].size != n) {
+            var stepHalf = step % 2
+            if (step % 2 == 0) {
+                moveSingle(oddSteps[stepHalf % 3], oddSteps[(stepHalf + 1) % 3])
+            } else {
+                moveSingle(evenSteps[stepHalf % 3], evenSteps[(stepHalf + 1) % 3])
+            }
+            step++
+        }
+    }
 }
 
 fun main() {
     var hnt = HanNuoTa(5)
-    hnt.moveAToC(5)
+    // hnt.moveAToC(5)
+
+    // hnt.moveLikeACircle(5)
+    hnt.moveLikeACircle(5)
 }
