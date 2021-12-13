7cf414da406a5e069ce7ef77be9e54d91c130832
proffl <157150278@.qq.com>
2021-12-13 17:17:31 +0800

    CodingInterviewGuide: deep copy tree

--- Interview/CodingInterviewGuide/src/main/kotlin/link/CopyTreeToNode.kt
+++ Interview/CodingInterviewGuide/src/main/kotlin/link/CopyTreeToNode.kt
@@ -3,17 +3,40 @@ package link
 import struct.TreeNode
 import struct.TwoNode
 
+/**
+ * @content copy a tree
+ *
+ * @level 2
+ */
 class CopyTreeToNode {
     fun copy(root: TreeNode): TwoNode {
         var head = TwoNode(root.value)
-        var stack = head
+        var queue = ArrayDeque<TreeNode>()
+        var queue2 = ArrayDeque<TwoNode>()
+
+        queue.add(root)
+        queue2.add(head)
+        while (!queue.isEmpty()) {
+            var cur = queue.removeFirst()
+            var newCur = queue2.removeFirst()
+            if (cur.leftNode != null) {
+                queue.add(cur.leftNode!!)
+                newCur.leftNode = TwoNode(cur.leftNode!!.value)
+                queue2.add(newCur.leftNode!!)
+            }
+            if (cur.rightNode != null) {
+                queue.add(cur.rightNode!!)
+                newCur.rightNode = TwoNode(cur.rightNode!!.value)
+                queue2.add(newCur.rightNode!!)
+            }
+        }
 
         return head
     }
 }
 
 fun main() {
-    var treeNode = Array<TreeNode>(11) { i -> TreeNode(i) }
+    var treeNode = Array(11) { i -> TreeNode(i) }
 
     treeNode[6].leftNode = treeNode[4]
     treeNode[6].rightNode = treeNode[7]
@@ -25,6 +48,9 @@ fun main() {
     treeNode[9].leftNode = treeNode[8]
 
     println(treeNode[6])
+
+    var copyTree = CopyTreeToNode()
+    println(copyTree.copy(treeNode[6]))
 }
 
 
--- Interview/CodingInterviewGuide/src/main/kotlin/struct/TwoNode.kt
+++ Interview/CodingInterviewGuide/src/main/kotlin/struct/TwoNode.kt
@@ -1,4 +1,11 @@
 package struct
 
-class TwoNode(var value: Int, var leftNode: TreeNode?=null, var rightNode: TreeNode?=null) {
+class TwoNode(var value: Int, var leftNode: TwoNode?=null, var rightNode: TwoNode?=null) {
+
+    override fun toString(): String {
+        var left = if (leftNode != null) leftNode!!.value.toString() else ""
+        var right = if (rightNode != null) rightNode!!.value.toString() else ""
+
+        return "value: $value, left: $left, right: $right"
+    }
 }
