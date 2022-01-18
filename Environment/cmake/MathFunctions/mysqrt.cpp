64dc0470f39ab87551cc13aa2830edfd64f13d3f
proffl <157150278@qq.com>
2022-01-18 16:04:34 +0800

    cmake tutorial check_symbol

--- Environment/cmake/MathFunctions/mysqrt.cpp
+++ Environment/cmake/MathFunctions/mysqrt.cpp
@@ -1,6 +1,14 @@
 #include <math.h>
 #include "MathFunctions.h"
+#include <cmath>
 
-double mysqrt(int inputValue) {
-    return sqrt(inputValue);
+double mysqrt(int x) {
+    #if defined(HAVE_LOG) && defined(HAVE_EXP)
+    double result = exp(log(x) * 0.5);
+    std::cout << "Computing sqrt of " << x << " to be " << result
+                << " using log and exp" << std::endl;
+    #else
+    double result = sqrt(x);
+    #endif
+    return result;
 }
