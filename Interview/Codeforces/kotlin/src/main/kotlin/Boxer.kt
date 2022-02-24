d71d755dc1609f1d604ddb0798703f14fa2a2bef
proffl <157150278@qq.com>
2022-02-25 00:27:11 +0800

    kotlin boxer...

--- Interview/Codeforces/kotlin/src/main/kotlin/Boxer.kt
+++ Interview/Codeforces/kotlin/src/main/kotlin/Boxer.kt
@@ -3,50 +3,50 @@ import java.util.*
 
 fun main(args: Array<String>) {
     fun solve(input: Scanner) {
-        val MAX = 10
+        val MAX = 150000
         val numCounts = input.nextInt()
-        val numArr = Array(MAX + 1) { 0 }
-        val bakArr = Array(MAX + 1) { 0 }
+        val reachedArr = Array(MAX + 1) { 0 }
+        val realArr = Array(MAX + 1) { 0 }
         var maxCount = 0
+
         for (i in 0 until numCounts) {
             val num = input.nextInt() - 1
-            if (num > MAX || num < 0) {
-                continue
-            }
-            bakArr[num] = 1
-            if (numArr[num] == 0) {
-                numArr[num] = 1
+            realArr[num] = 1
+
+            if (reachedArr[num] == 0) {
+                reachedArr[num] = 1
                 maxCount++
             } else {
-                var k = num + 1
-                while (k <= MAX && bakArr[k] == 1) {
-                    k++
+                var j = num - 1
+                while (j > 1 && realArr[j] == 1) {
+                    j--
                 }
-                if (numArr[k] == 0) {
-                    numArr[k] = 1
+                if (num > 1 && reachedArr[j] == 0) {
+                    reachedArr[j] = 1
                     maxCount++
                 } else {
-                    k = num - 1
-                    while (k >= 0 && bakArr[k] == 1) {
-                        k--
+                    j = num + 1
+                    while (j < MAX && realArr[j] == 1) {
+                        j++
                     }
-                    if (k >= 0 && numArr[k] == 0) {
-                        numArr[k] = 1
+                    if (reachedArr[j] == 0) {
+                        reachedArr[j] = 1
                         maxCount++
                     }
                 }
             }
         }
+
         println(maxCount)
     }
 
 
-//    val input = Scanner(System.`in`)
-//    solve(input)
-    val input =
-        Scanner(FileInputStream("src/main/resources/boxer_data"))
-    val testCase = input.nextInt()
-    for (i in 0 until testCase) {
-        solve(input)
-    }
+    val input = Scanner(System.`in`)
+    solve(input)
+//    val input =
+//        Scanner(FileInputStream("src/main/resources/boxer_data"))
+//    val testCase = input.nextInt()
+//    for (i in 0 until testCase) {
+//        solve(input)
+//    }
 }
