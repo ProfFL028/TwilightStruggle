5025050df7ffa220ed7921dbf2659ecfb6ff324e
proffl <157150278@qq.com>
2022-02-25 11:46:43 +0800

    kotlin heroes: finally complete ..

--- Interview/Codeforces/kotlin/src/main/kotlin/Boxer.kt
+++ Interview/Codeforces/kotlin/src/main/kotlin/Boxer.kt
@@ -5,35 +5,40 @@ fun main(args: Array<String>) {
     fun solve(input: Scanner) {
         val MAX = 150000
         val numCounts = input.nextInt()
-        val reachedArr = Array(MAX + 1) { 0 }
-        val realArr = Array(MAX + 1) { 0 }
-        var maxCount = 0
-
+        val numArray = Array(MAX + 1) { 0 }
+        val resultArray = Array(MAX + 1) { 0 }
         for (i in 0 until numCounts) {
             val num = input.nextInt() - 1
-            realArr[num] = 1
-
-            if (reachedArr[num] == 0) {
-                reachedArr[num] = 1
+            if (num in 0..MAX) {
+                numArray[num] += 1
+            }
+        }
+        var maxCount = 0
+        if (numArray[0] >= 1) {
+            maxCount += 1
+            numArray[0] -= 1
+            resultArray[0] = 1
+        }
+        if (numArray[0] >= 1) {
+            maxCount += 1
+            numArray[0] -= 1
+            resultArray[1] = 1
+        }
+        for (i in 1..MAX) {
+            if (resultArray[i - 1] == 0 && numArray[i] >= 1) {
                 maxCount++
-            } else {
-                var j = num - 1
-                while (j > 1 && realArr[j] == 1) {
-                    j--
-                }
-                if (num > 1 && reachedArr[j] == 0) {
-                    reachedArr[j] = 1
-                    maxCount++
-                } else {
-                    j = num + 1
-                    while (j < MAX && realArr[j] == 1) {
-                        j++
-                    }
-                    if (reachedArr[j] == 0) {
-                        reachedArr[j] = 1
-                        maxCount++
-                    }
-                }
+                numArray[i] -= 1
+                resultArray[i - 1] = 1
+            }
+            if (resultArray[i] == 0 && numArray[i] >= 1) {
+                maxCount++
+                numArray[i] -= 1
+                resultArray[i] = 1
+            }
+            if (numArray[i] >= 1) {
+                maxCount++
+                numArray[i] -= 1
+                resultArray[i + 1] = 1
             }
         }
 
