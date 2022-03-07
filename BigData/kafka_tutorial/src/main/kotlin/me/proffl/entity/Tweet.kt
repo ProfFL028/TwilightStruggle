f7f3172b72ec02056da43184acfc963f9eefcfe7
proffl <157150278@qq.com>
2022-03-07 23:05:58 +0800

    filter, merge, sink ..

--- BigData/kafka_tutorial/src/main/kotlin/me/proffl/entity/Tweet.kt
+++ BigData/kafka_tutorial/src/main/kotlin/me/proffl/entity/Tweet.kt
@@ -4,14 +4,30 @@ import com.google.gson.annotations.SerializedName
 
 data class Tweet(
     @SerializedName("CreatedAt")
-    var createdAt: Long,
+    var createdAt: Long = 0L,
     @SerializedName("Id")
-    var id: Long,
+    var id: Long = 0L,
     @SerializedName("Lang")
-    var lang: String,
+    var lang: String = "",
     @SerializedName("Retweet")
-    var retweet: Boolean,
+    var retweet: Boolean = false,
     @SerializedName("Text")
-    var text: String
+    var text: String = ""
 ) {
+}
+
+class DummyTranslate {
+    fun translate(tweet: Tweet?): Tweet {
+        if (tweet == null) {
+            return Tweet()
+        }
+        val translatedTweet = Tweet()
+        translatedTweet.id = tweet.id
+        translatedTweet.createdAt = tweet.createdAt
+        translatedTweet.lang = "en"
+        translatedTweet.retweet = tweet.retweet
+        translatedTweet.text = "Translated-----${tweet.text}---"
+
+        return translatedTweet
+    }
 }
