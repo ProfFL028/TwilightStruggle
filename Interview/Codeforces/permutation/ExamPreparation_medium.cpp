7eab0db5a3269b64748f46b6a56ec9ec496f46ef
proffl <157150278@qq.com>
2022-04-26 08:10:15 +0800

    permutation: backup

--- Interview/Codeforces/permutation/ExamPreparation_medium.cpp
+++ Interview/Codeforces/permutation/ExamPreparation_medium.cpp
@@ -39,24 +39,64 @@ int C(int x, int y) {
     }
     return ans;
 }
+
+int simple(int a, int b, int k) {
+    if (k == 0 || b == 0 || a == 0) {
+        return 1;
+    }
+    if (a <= k) {
+        return C(a + b, a);
+    }
+    if (a >= (k * b)) {
+        return myPower(b, k + 1);
+    }
+    return -1;
+}
+
 void solve() {
     int a, b, k;
     cin >> a >> b >> k;
 
-    if (k == 0 || b == 0 || a == 0) {
-        cout << 1 << endl;
-    } else if (a <= k) {
-        cout << C(a + b, a) << endl;
-    } else if (a >= (k * b)) {
-        cout << myPower(b, k + 1) << endl;
-    } else {
-        int ak = a / k;
-        int remain = a % k;
-        int slot = (k + 1) * ak + remain;
-        if (remain != 0)
-            b += 1;
-        cout << C(slot + b, b) << endl;
+
+    stack<tuple<int, int, int, int>> s;
+    map<tuple<int, int, int>, int> dp;
+
+    s.emplace(a, b, k, 1);
+    while (!s.empty()) {
+        auto x = s.top();
+        s.pop();
+        tuple<int, int, int> abk = tuple<int, int, int>(get<0>(x), get<1>(x), get<2>(x));
+        if (dp.find(abk) == dp.end()) {
+            s.push(x);
+            ll result = 0;
+            bool solved = true;
+            int ak = get<0>(x) / get<2>(x);
+            for (int i = 0; i <= ak; i++) {
+                int ai = get<0>(x) - i * get<2>(x);
+                int bi = get<1>(x) - i;
+                int ki = get<2>(x) - 1;
+                tuple<int, int, int> abki(ai, bi, ki);
+                if (dp.find(abki) == dp.end()) {
+                    int tmp = simple(ai, bi, ki);
+                    if (tmp != -1) {
+                        dp[abki] = tmp;
+                        result = (result + dp[abki] * C(bi, i)) % M;
+                    } else {
+                        solved = false;
+                        if (i <= get<1>(x)) {
+                            s.emplace(ai, bi, ki, C(get<1>(x), i));
+                        }
+                    }
+                }
+            }
+            if (solved) {
+                dp[abk] = result;
+                s.pop();
+            }
+        }
     }
+
+    cout << dp[tuple<int, int, int>(a, b, k)] << endl;
 }
 
 int main() {
