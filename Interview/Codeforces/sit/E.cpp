4448ab2297d475cc4d1be30ee2a2e5825ab92afb
profFL <157150278@qq.com>
2022-04-26 23:11:17 +0800

    codeforces sit e

--- Interview/Codeforces/sit/E.cpp
+++ Interview/Codeforces/sit/E.cpp
@@ -19,18 +19,36 @@ void solve() {
     for (int i = 0; i < n; i++) {
         cin >> arr[i];
     }
-    map<int, map<int, set<int>>> m;
+    map<int, map<int, map<int, int>>> m;
     for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
+        for (int j = 0; j < k; j++) {
             char x = arr[i][j];
-            if (m.count(x) == 1) {
-                m[x][i].insert(j);
-            }
+            m[x][i][j] = 1;
         }
     }
     int ans = 0;
-    for (auto& kv : m) {
-        auto& v = kv.second;
+    for (auto &kv: m) {
+        auto v = kv.second;
+        auto e1 = v.begin();
+        while (e1 != v.end()) {
+            auto e2 = e1;
+            e2++;
+            while (e2 != v.end()) {
+                int c = 0;
+                for (auto l: e1->second) {
+                    if (l.second == 1) {
+                        if (e2->second[l.first] == 1) {
+                            c++;
+                        }
+                    }
+                }
+                if (c >= 2) {
+                    ans += (c * (c - 1)) / 2;
+                }
+                e2++;
+            }
+            e1++;
+        }
     }
 
     cout << ans << endl;
