390bfd976fc870bcc41014c52889695175804238
profFL <157150278@qq.com>
2022-05-11 00:09:51 +0800

    codeforces 789#div b

--- Interview/Codeforces/strings/Good01String1_simple.cpp
+++ Interview/Codeforces/strings/Good01String1_simple.cpp
@@ -1,37 +1,31 @@
-// codeforces: https://codeforces.com/contest/1678/problem/A
+// codeforces:  https://codeforces.com/contest/1678/problem/B1
 
 #include <bits/stdc++.h>
 
 using namespace std;
 
-//gcc optimization
+// gcc optimization (it may doesn't work)
 // #pragma GCC optimize("Ofast")
 // #pragma GCC optimize "trapv"
 #define fast(); ios_base::sync_with_stdio(false); cin.tie(NULL);
 typedef long long ll;
 typedef unsigned long long ull;
 typedef long double lld;
-const int MOD = 1e9 + 7;
-#define print(v) cout << v.size() << endl; for (auto& v: result) cout << v << " "; cout << endl;
+const ll MOD = 1000000007;
+#define print(v) cout << v.size(); for (auto& x: v) cout << x << " "; cout << endl;
 
 void solve() {
-    int n;
-    cin >> n;
-    map<int, int> m;
-    bool containSame = false;
-    for (int i = 0; i < n; i++) {
-        int x;
-        cin >> x;
-        m[x]++;
-        if (m[x] > 1) {
-            containSame = true;
+    int n, L = -1, x = 0, y = 0;
+    string s;
+    cin >> n >> s;
+    for (int i = 0; i < n; i += 2) {
+        if (s[i] != s[i + 1]) x += 1;
+        else {
+            if (L != s[i]) y += 1;
+            L = s[i];
         }
     }
-    int ans = n - m[0];
-    if (m[0] == 0 && !containSame) {
-        ans++;
-    }
-    cout << ans << endl;
+    cout << x << " " << max(y, 1) << "\n";
 }
 
 int main() {
