9b62a46f27729459950f82cd3e0e05b8a167f7a7
proffl <157150278@qq.com>
2022-06-07 17:31:09 +0800

    codeforces: 796#div2, AB

---
+++ Interview/Codeforces/bitwise/Patchouli_Magical_Talisman_800.cpp
@@ -0,0 +1,59 @@
+// codeforces: https://codeforces.com/contest/1688/problem/B
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+//gcc optimization
+// #pragma GCC optimize("Ofast")
+// #pragma GCC optimize "trapv"
+#define fast(); ios_base::sync_with_stdio(false); cin.tie(NULL);
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+const int MOD = 1e9 + 7;
+#define print(v) cout << v.size() << endl; for (auto& v: result) cout << v << " "; cout << endl;
+
+void solve() {
+    int n;
+    cin >> n;
+    vector<int> arr;
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        int k = 0;
+        while ((x & 1) ^ 1) {
+            k++;
+            x >>= 1;
+        }
+        if (k > 0) {
+            arr.push_back(k);
+        }
+    }
+    sort(arr.begin(), arr.end());
+
+    int ans = 0;
+
+
+    cout << ans << endl;
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("../input.txt", "r", stdin);
+#endif
+    fast()
+
+    int testCase = 1;
+    cin >> testCase;
+    while (testCase > 0) {
+        solve();
+        testCase--;
+    }
+
+#ifndef ONLINE_JUDGE
+    cout << "\nTime Elapsed : " << 1000 * (lld) clock() / (lld) CLOCKS_PER_SEC
+         << " ms\n";
+#endif
+    return 0;
+}
---
+++ Interview/Codeforces/bitwise/PerfectBitmaskClass_800.cpp
@@ -0,0 +1,55 @@
+// codeforces: https://codeforces.com/contest/1688/problem/A
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+//gcc optimization
+// #pragma GCC optimize("Ofast")
+// #pragma GCC optimize "trapv"
+#define fast(); ios_base::sync_with_stdio(false); cin.tie(NULL);
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+const int MOD = 1e9 + 7;
+#define print(v) cout << v.size() << endl; for (auto& v: result) cout << v << " "; cout << endl;
+
+void solve() {
+    int n;
+    cin >> n;
+    if (n == 1) {
+        cout << "3" << endl;
+        return ;
+    }
+
+    int ans = 1;
+    while ((n & 1) ^ 1) {
+        ans *= 2;
+        n >>= 1;
+    }
+    if (n<=1) {
+        ans += 1;
+    }
+
+    cout << ans << endl;
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+    freopen("../input.txt", "r", stdin);
+#endif
+    fast()
+
+    int testCase = 1;
+    cin >> testCase;
+    while (testCase > 0) {
+        solve();
+        testCase--;
+    }
+
+#ifndef ONLINE_JUDGE
+    cout << "\nTime Elapsed : " << 1000 * (lld) clock() / (lld) CLOCKS_PER_SEC
+         << " ms\n";
+#endif
+    return 0;
+}
