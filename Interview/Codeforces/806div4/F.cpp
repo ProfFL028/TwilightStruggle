334766430c7ca6aedaed5ec610a1ec85883eb419
proffl <157150278@qq.com>
2022-08-01 14:31:02 +0800

    codeforces 806div4

--- Interview/Codeforces/806div4/F.cpp
+++ Interview/Codeforces/806div4/F.cpp
@@ -14,16 +14,28 @@ typedef long double lld;
 const int MOD = 1e9 + 7;
 #define print(v) cout << v.size() << endl; for (auto& v: result) cout << v << " "; cout << endl;
 
+const int MM = 1e9+2;
 void solve() {
     int n;
     cin >> n;
-    vector<int> arr(n, 0);
-    for (int i = 0; i < n; i++) {
-        cin >> arr[i];
+    vector<int> arr;
+    vector<int> left(MM, 0);
+    for (int i = 1; i <= n; i++) {
+        int x;
+        cin >> x;
+        if (i > x)  {
+            arr.push_back(i);
+            left[x]++;
+        }
+    }
+    for (int i = MM-2; i>=0; i--) {
+        left[i] += left[i+1];
     }
 
-    int ans = 0;
+    ll ans = 0;
+    for (auto x : arr) {
 
+    }
     cout << ans << endl;
 }
 
